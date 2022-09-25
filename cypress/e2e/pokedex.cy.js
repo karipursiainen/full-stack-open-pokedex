5c2f9035f5739f57622702a34fd475151c685c90
karipursiainen <kari_pursiainen@hotmail.com>
2022-09-25 14:51:23 +0300

    Update pokedex.cy.js

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,7 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cypress.visit('http://localhost:5000')
-    cypress.contains('ivysaur')
-    cypress.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
